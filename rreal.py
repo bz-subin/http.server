cec80167c38f961e11af88bf589a6eb8fce59b37
김수빈 <bz.subin@gmail.com>
2026-01-16 16:46:06 +0900

    중간저장

---
+++ rreal.py
@@ -0,0 +1,36 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+#http안에 있는 server에서 HTTPServer이라는것과 BaseHTTPRequestHandler을 가져온다.
+class MyRequestHendler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("content-type","text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("<h1>가나다</h1>".encode())
+        a = self.requestline
+        self.wfile.write("<body>".encode()
+        +"<h2>".encode()+a.encode()+"</h2>".encode() #h2 a(로그)
+        +"<h2>클릭</h2>".encode()+ #h2 클릭
+        "</body>".encode())
+        self.wfile.write("")
+
+
+
+
+
+
+
+#BaseHTTPRequestHandler을 받아와서 MyRequestHandler라는 클래스를 실행한다.
+#(서버 상태, 초기 설정, 실제 입력 내용)
+
+host = "localhost"
+port = 800
+#
+
+#인스턴트
+servers = HTTPServer((host,port), MyRequestHendler)
+print(f"서버가 시작되었습니다. http://{host}:{port}로 접속하세요!")
+
+try:
+    servers.serve_forever()
+except KeyboardInterrupt:
+    servers.server_close()
