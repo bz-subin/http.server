1dea124ed4b047190b54a7411c915c1a0e135db1
김수빈 <bz.subin@gmail.com>
2026-01-17 19:22:31 +0900

    계속 rfile 시도중

--- rreal.py
+++ rreal.py
@@ -1,23 +1,22 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyRequestHendler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("content-type","text/html; charset=utf-8")
-        self.end_headers()
-        self.wfile.write("<h1>가나다</h1>".encode())
+    def do_GET(self): # 대놓고 보여줌
+        self.send_response(200) #서버 상태
+        self.send_header("content-type","text/html; charset=utf-8") #줄 내용 속성 설정
+        self.end_headers() # 내용 설정 완료!
+        self.wfile.write("<h1>가나다</h1>".encode()) # 보내줄 내용
         
-    def do_POST(self, re_length):
-        self.headers("content-length","text/html; charset=utf-8")
-        re_length = int("content-length")
-        self.rfile.read(re_length.decode())
-        print(re_length)
-        self.send_response(200)
-        self.send_header("content-type","text/html; charset=utf-8")
-        self.end_headers()
-        self.wfile.write(re_length.encode())
-        print(re_length)
+    def do_POST(self, con_length): # 숨겨서 보여줌(아이디, 비밀번호)
+        self.headers("content-length")
+        self.rfile.read("content-length".decode())
+        con_length = "content-length"
+        con_length = int(con_length) 
+        print(con_length)
 
+        self.send_response(200)
+        self.wfile.write(re_length.encode())
+        
 host = "localhost"
 port = 8001
 
