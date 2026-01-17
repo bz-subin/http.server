cd968e8f26e8846f8b078354e022dccc9b3143c9
김수빈 <bz.subin@gmail.com>
2026-01-17 16:45:14 +0900

    Docs: post 수정

--- rreal.py
+++ rreal.py
@@ -1,7 +1,7 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 #http안에 있는 server에서 HTTPServer이라는것과 BaseHTTPRequestHandler을 가져온다.
 class MyRequestHendler(BaseHTTPRequestHandler):
-    def do_post(self):
+    def do_POST(self):
         self.send_response(200)
         self.send_header("content-type","text/html; charset=utf-8")
         self.end_headers()
