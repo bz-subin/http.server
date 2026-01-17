82e5aef8075ca20a03c42e8a725a89d3e8908c88
김수빈 <bz.subin@gmail.com>
2026-01-17 19:54:30 +0900

    시도중.

--- rreal.py
+++ rreal.py
@@ -7,18 +7,32 @@ class MyRequestHendler(BaseHTTPRequestHandler):
         self.end_headers() # 내용 설정 완료!
         self.wfile.write("<h1>가나다</h1>".encode()) # 보내줄 내용
         
-    def do_POST(self, con_length): # 숨겨서 보여줌(아이디, 비밀번호)
-        self.headers("content-length")
-        self.rfile.read("content-length".decode())
-        con_length = "content-length"
+
+
+
+    def do_POST(self): # 숨겨서 보여줌(아이디, 비밀번호)
+        print("췍췍")
+        self.send_response(200)
+        con_length = self.end_headers.get("content-length") #.get를 써야한다고 함
+        self.rfile.read(con_length.decode())
+        print(con_length)
         con_length = int(con_length) 
         print(con_length)
 
-        self.send_response(200)
-        self.wfile.write(re_length.encode())
+        self.wfile.write(
+        """
+        <body>
+        <form action="/" method="POST">
+            <input type="text" name="my_msg"> 
+            
+            <input type="submit" value="보내기">
+        </form>
+        </body>
+        """
+        .encode())
         
 host = "localhost"
-port = 8001
+port = 8002
 
 servers = HTTPServer((host,port), MyRequestHendler)
 print(f"서버가 시작되었습니다. http://{host}:{port}로 접속하세요!")
