47631f3021baa174d3fd2272caa1e8b1f7312e24
김수빈 <bz.subin@gmail.com>
2026-01-17 19:16:02 +0900

    rfile 시도중

--- rreal.py
+++ rreal.py
@@ -7,8 +7,19 @@ class MyRequestHendler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write("<h1>가나다</h1>".encode())
         
+    def do_POST(self, re_length):
+        self.headers("content-length","text/html; charset=utf-8")
+        re_length = int("content-length")
+        self.rfile.read(re_length.decode())
+        print(re_length)
+        self.send_response(200)
+        self.send_header("content-type","text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write(re_length.encode())
+        print(re_length)
+
 host = "localhost"
-port = 8000
+port = 8001
 
 servers = HTTPServer((host,port), MyRequestHendler)
 print(f"서버가 시작되었습니다. http://{host}:{port}로 접속하세요!")
@@ -19,7 +30,4 @@ try:
 except KeyboardInterrupt:
     servers.server_close()
 
-    # def do_POST(self, re_length):
-    #     self.headers("content-length","text/html; charset=utf-8")
-    #     re_length = int("content-length")
-    #     self.rfile.read(re_length.decode())
+
